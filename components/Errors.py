1a6f20580e461c115749f53d00f0f43dda77c63b
Jess <47252025+Dwyfy@users.noreply.github.com>
2021-09-12 13:06:43 -0600

    Update Errors.py

--- components/Errors.py
+++ components/Errors.py
@@ -15,3 +15,20 @@ class InvalidPasswordError(Error):
 
     def __init__(self):
         super().__init__(self.DEFAULT_MESSAGE)
+
+class UserAlreadyExistsError(Error):
+    #user already exists
+
+    DEFAULT_MESSAGE = "Error: user already exists"
+
+    def __init__(self):
+        super().__init__(self.DEFAULT_MESSAGE)
+
+class PasswordsDoNotMatch(Error):
+    #passwords do not math
+
+    DEFAULT_MESSAGE = "Error: The passwords you entered do not match"
+
+    def __init__(self):
+        super().__init__(self.DEFAULT_MESSAGE)
+
