2158cf0cd32a6e615d2b168e34157be642482989
David Hovey <david.hovey@va.gov>
2021-09-13 07:25:26 -0400

    registration handler mods

--- components/RegistrationHandler.py
+++ components/RegistrationHandler.py
@@ -1,38 +1,29 @@
 #handles writing new users
 
 #libraries
+import csv
 
 #custom classes
-from components.Hasher import Hasher
-from components.Errors import PasswordsDoNotMatch, UserAlreadyExistsError, PasswordsDoNotMatch
-import csv
+from utilities.Hasher import Hasher
+from utilities.Errors import PasswordsDoNotMatch, UserAlreadyExistsError, PasswordsDoNotMatch
+from utilities.TempUser import User
 
 class RegistrationHanlder:
 
     #global instance variables
+    users = []
     FILE_NAME="credentials.csv"
 
-    #def __init__(self, inputUserName, inputPassword):
-        #TODO: remove pass after implementation
-        #pass
+    def register(self, inputUser, inputPass):
+        #local variable assignments
+        self.userName = inputUser.lower()
+        self.password = inputPass
 
-    def register(self, inputUser, inputPass, inputConfirmPass):
-
-        userName = inputUser.lower()
-        password = inputPass
-        confirmPassword = inputConfirmPass
-
-        if (password == confirmPassword):
-            stringHashedPassword = Hasher.hash(password)
-        else:
-            raise PasswordsDoNotMatch()    
-
+        #hash the input
+        stringHashedPassword = Hasher.hash(self.password)
         #if user is not found add username & hashed password
-        self.checkForExistingUser(userName)
-        self.writeCredentials(userName, stringHashedPassword)
-
-        #TODO: remove pass after implementation
-        #driver function for the rest of the functions
+        self.checkForExistingUser(self.userName)
+        self.writeCredentials(self.userName, stringHashedPassword)
 
     def read_file(self, fileName):
         with open(fileName) as csv_file:
@@ -40,21 +31,17 @@ class RegistrationHanlder:
             line_count = 0
             for row in csv_reader:
                 #ensure username is lower case | strip whitespace out of password
-                loadedUser = self.User(loadedUsername = row[0].lower(), loadedPassword = row[1].strip(), userIndex = line_count)
+                loadedUser = User(loadedUsername = row[0].lower(), loadedPassword = row[1].strip(), userIndex = line_count)
                 self.users.append(loadedUser)
                 line_count += 1
 
     def writeCredentials(self, username, password):
-        #TODO: remove pass after implementation
-        
         #write the username and hashed pass to the file
         with open(self.FILE_NAME, mode='w') as userFiles:
             credential_writer = csv.writer(userFiles, delimiter=',', quotechar='"')
             credential_writer.writerow(username, password)
 
     def checkForExistingUser(self, username):
-        #TODO: remove pass after implementation
-        pass
         #check to see if the username exists already, if so raise a user already exists exception
         self.read_file(self.FILE_NAME)
         for user in self.users:
