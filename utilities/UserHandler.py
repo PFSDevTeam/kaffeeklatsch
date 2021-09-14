01a5c6e95e4b098331982215205481f8ed28f80e
David Hovey <david.hovey@va.gov>
2021-09-14 08:47:58 -0400

    added primary key and nullable values

--- utilities/UserHandler.py
+++ utilities/UserHandler.py
@@ -14,7 +14,7 @@ class UserHandler:
     #DB Creation
     # user_db = sqlite3.connect('db_files/users.db')
     # db_cursor = user_db.cursor()
-    # db_cursor.execute("CREATE TABLE users (username text, password text)")
+    # db_cursor.execute("CREATE TABLE users (username TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL)")
     # db_cursor.execute("INSERT INTO users VALUES ('steve', '432c19c7ecdc9ef6884d01b21bd306ddd4b21d4ee139f10358cf5c1b7f113904')")
     # user_db.commit()
     # db_cursor.execute("SELECT * FROM users")
@@ -47,11 +47,11 @@ class UserHandler:
             db_cursor.execute("SELECT * FROM users WHERE username=:username", {'username': inputUsername})
             results = db_cursor.fetchone()
             if not (results == None):
-                #results are present
+                #user found
                 requestedUser = User(loadedUsername=results[0].lower(), loadedPassword=results[1].strip(), userIndex=0)
                 return requestedUser
             else:
-                results
+                #user was not found
                 return None
 
     @classmethod
