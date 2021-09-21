b07e0a4a3336b283e1f7ac596647887076710f81
David Hovey <david.hovey@va.gov>
2021-09-21 19:47:47 -0400

    make post element positioning

--- forms/MakePostForm.py
+++ forms/MakePostForm.py
@@ -2,5 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class MakePostForm(FlaskForm):
+    postTitle = StringField('post title')
     postContent = StringField('post content')
     post = SubmitField('post')
