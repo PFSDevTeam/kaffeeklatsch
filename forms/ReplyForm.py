4e721df27ca136f37acbf2ebff986c093abc3877
redgolf <89739854+redgolf@users.noreply.github.com>
2021-09-20 19:14:23 -0500

    Updated style some more

--- forms/ReplyForm.py
+++ forms/ReplyForm.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField
 
 class ReplyForm(FlaskForm):
     
-    post = SubmitField('Reply')
-    ownerName = StringField('John Doe')
-    communityInfo = StringField('Community Name')
-    userInfo = StringField('User Info')
-    postTitle = StringField('Post Title')
-    ownerContent = StringField('All of the news..')
-    replyContent = StringField('Reply Content')
-    upArrow = SubmitField('Amazing')
-    downArrow = SubmitField('Mediocre')
+    post = SubmitField('post')
+    like = SubmitField('like')
+    ownerName = StringField('owner name', default='John Doe')
+    communityInfo = StringField('community', default='Community Name')
+    userInfo = StringField('user', default='User Info')
+    postTitle = StringField('post', default='Post Title')
+    ownerContent = StringField('owner', default='All of the news..')
+    replyContent = StringField('reply', default='Reply Content')
+    upArrow = SubmitField('↑')
+    downArrow = SubmitField('↓')
