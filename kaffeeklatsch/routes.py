df7818079d59f6074f5d0b6dee5b3ad58bd68dc1
David Hovey <david.hovey@va.gov>
2021-09-27 12:19:37 -0400

    completed migration to sqalchemy

--- kaffeeklatsch/routes.py
+++ kaffeeklatsch/routes.py
@@ -27,33 +27,25 @@ def login():
   loginHandler = LoginHandler()
   form = LoginForm()
 
-#   try:
-#     #NOTE: At this point the validLogin function either raises an error or returns true, consider removing boolean return
-#     validLogin = loginHandler.login("steve", "scuba")
-#     # validLogin = loginHandler.login("scuba", "steven")
-#     # validLogin = loginHandler.login("jeff", "guy")
-#     print(f'valid login: ' + str(validLogin))
-#   except InvalidUsernameError as err:
-#     print(err)
-#   except InvalidPasswordError as err:
-#     print(err)
-#   except UserNotFoundError as err:
-#     print(err)
-
   #LOGIN IMPLEMENTATION BEGINS
   if form.validate_on_submit():
     #flash messaging doesn't currently work 
     # flash(f'login successfull!', 'success')
-    # session["username"] = request.form['username']
-    if (loginHandler.login(form.username.data, form.password.data)):
-      user = User.query.filter_by(username=form.username.data).first()
+    try:
+      if (loginHandler.login(form.username.data, form.password.data)):
+          user = User.query.filter_by(username=form.username.data).first()
       login_user(user)
-    print(f'retrived user: {user}')
-    return redirect(url_for('feed'))
+      print(f'retrived user: {user}')
+      return redirect(url_for('feed'))
+    except InvalidUsernameError as err:
+      print(err)
+    except InvalidPasswordError as err:
+      print(err)
+    except UserNotFoundError as err:
+      print(err)
   else:
     print(form.errors)
     print("form input is incorrect")
-
 
   #render the html template 
   return render_template('login.html', form=form)
@@ -64,8 +56,6 @@ def login():
 def register():
   registrationHandler = RegistrationHandler()
   registrationForm = RegistrationForm()
-  #testing 
-  loginHandler = LoginHandler()
   
   #LOGIN IMPLEMENTATION BEGINS
   if registrationForm.validate_on_submit():
@@ -73,38 +63,17 @@ def register():
     inputUsername = request.form['username']
     inputPassword = request.form['password']
     registrationHandler.register(inputUsername, inputPassword)
-    validLogin = loginHandler.login(inputUsername, inputPassword)
-    print(f'was our login successful?: {validLogin}')
 
     #REDIRECT TO LOGIN
     return redirect(url_for('login'))
   else:
     print("form input is incorrect")
-
-      #REGISTRATION TESTING
-  # regHandler = RegistrationHandler()
-  # try:
-  #   # regHandler.register("steve", "scuba")
-  #   regHandler.register("scuba", "steven")
-  #   # regHandler.register("jeff", "guy")
-  #   print('registered')
-  # except UserAlreadyExistsError as err:
-  #   print(err)
   
   return render_template('register.html', form=registrationForm)
 
 @app.route('/feed')
 def feed():
 
-    #DB TEST
-    # print(f'users modesl list: {UserAccess.query.all()}')
-    # print(f'users modesl list: {User.query.all()}')
-    # print(f'community model list: {Community.query.all()}')
-    # print(f'post model list: \n {Post.query.all()}')
-      
-    #check session cookies, if it's not set redirect to login
-    # if (session.get("username") == None):
-    #     return redirect(url_for('login'))
     if (current_user.is_authenticated == False):
       return redirect(url_for('login')) 
 
--- kaffeeklatsch/utilities/UserHandler.py
+++ kaffeeklatsch/utilities/UserHandler.py
@@ -1,34 +1,17 @@
 # the user handler class handles the repetative functions for the Login and registration functions
 
 #libraries
-import sqlite3
 from kaffeeklatsch.utilities.Errors import UserNotFoundError
-
-#custom classes
-from kaffeeklatsch.utilities.TempUser import User
+from kaffeeklatsch.models.models import UserAccess, User
+from kaffeeklatsch import db
 
 class UserHandler:
-
-    #DB Creation (uncomment when needed)
-    # user_db = sqlite3.connect('db_files/application_data.db')
-    # db_cursor = user_db.cursor()
-    # db_cursor.execute("CREATE TABLE user_access (username TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL)")
-    # #sample data
-    # db_cursor.execute("INSERT INTO user_access VALUES ('steve', '432c19c7ecdc9ef6884d01b21bd306ddd4b21d4ee139f10358cf5c1b7f113904')")
-    # user_db.commit()
-    # db_cursor.execute("SELECT * FROM user_access")
-    # print(f'resutls: {db_cursor.fetchone()}')
-    # user_db.close()
-
-        ##check user block
 
     #check if user exists, if so return true
     @classmethod
     def checkUserExists(cls, username):
         #check if the username is present
         foundUser = cls.__getSelectedUser(username)
-        #empty the users array
-        cls.users = []
         if (foundUser !=  None):
             return True
         else:
@@ -38,27 +21,16 @@ class UserHandler:
     @classmethod
     def __getSelectedUser(cls, inputUsername):
             #try to find the username from the loaded users
-        #db commection
-        user_db = sqlite3.connect("kaffeeklatsch/db_files/application_data.db")
-        db_cursor = user_db.cursor()
-        with user_db:
-            #run query
-            db_cursor.execute("SELECT * FROM user_access WHERE username=:username", {'username': inputUsername})
-            results = db_cursor.fetchone()
-            if not (results == None):
-                #user found
-                requestedUser = User(loadedUsername=results[0].lower(), loadedPassword=results[1].strip(), userIndex=0)
-                return requestedUser
-            else:
-                #user was not found
-                return None
+        user = UserAccess.query.filter_by(username=inputUsername).first()
+        if (user != None):
+            return user
+        else:
+            return None
     
     @classmethod
     def getUser(cls, username):
         #check if the username is present
         foundUser = cls.__getSelectedUser(username)
-        #empty the users array
-        cls.users = []
         if (foundUser !=  None):
             return foundUser
         else:
@@ -67,14 +39,12 @@ class UserHandler:
     #insert a new user into the database
     @classmethod
     def insertUser(cls, username, password):
-            user_db = sqlite3.connect("kaffeeklatsch/db_files/application_data.db")
-            db_cursor = user_db.cursor()
-            with user_db:
-                try:
-                    db_cursor.execute("INSERT INTO users VALUES (:username, :pass)", {'username': username, 'pass': password})
-                    user_db.commit()
-                    # db_cursor.execute("SELECT * FROM users")
-                    # print(f'resutls: {db_cursor.fetchall()}')
-                    return True
-                except:
-                    return False
+        newUserAccess = UserAccess(username=username, password=password)
+        newUserProfile = User(username=username)
+        try:
+            db.session.add(newUserAccess)
+            db.session.add(newUserProfile)
+            db.session.commit()
+            return True
+        except: 
+            return False
