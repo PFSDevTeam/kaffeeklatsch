f8c2124791178b1c5893a73ba01916070609cb41
David Hovey <david.hovey@va.gov>
2021-10-03 22:10:36 -0400

    restored input validation and defaults

--- kaffeeklatsch/forms/MakePostForm.py
+++ kaffeeklatsch/forms/MakePostForm.py
@@ -3,6 +3,6 @@ from wtforms.validators import DataRequired, Length
 from wtforms import StringField, SubmitField
 
 class MakePostForm(FlaskForm):
-    postTitle = StringField('postTitle')
-    postContent = StringField('post-content')
+    postTitle = StringField('post title', default='post title', validators=[DataRequired(), Length(min=1, max=50)])
+    postContent = StringField('post content', default='Start your Discourse...', validators=[DataRequired(), Length(min=1, max=250)])
     post = SubmitField('post')
