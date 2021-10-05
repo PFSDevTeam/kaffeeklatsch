c4fed76dedd2f3e0ccca3b70bbf480007945a655
gjfloding <geoffrey.floding@gmail.com>
2021-10-05 13:02:48 -0400

    Updated SQL File

--- kaffeeklatsch/db_files/application_data.db.sql
+++ kaffeeklatsch/db_files/application_data.db.sql
@@ -20,6 +20,16 @@ CREATE TABLE IF NOT EXISTS "post" (
 	"UUID"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("UUID")
 );
+DROP TABLE IF EXISTS "reply";
+CREATE TABLE IF NOT EXISTS "reply" (
+	"reply_UUID"	INTEGER NOT NULL UNIQUE,
+	"original_post_id"	INTEGER NOT NULL,
+	"reply_content"	TEXT NOT NULL,
+	"reply_user"	TEXT NOT NULL,
+	"reply_date"	TEXT NOT NULL,
+	PRIMARY KEY("reply_UUID"),
+	FOREIGN KEY("original_post_id") REFERENCES "post"("UUID")
+);
 DROP TABLE IF EXISTS "user";
 CREATE TABLE IF NOT EXISTS "user" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -29,17 +39,10 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"last_name"	TEXT,
 	"communities"	TEXT,
 	"users_following"	TEXT,
+	"tagline"	TEXT,
+	"date_joined"	TEXT NOT NULL,
+	"summary"	TEXT,
 	PRIMARY KEY("id")
-);
-DROP TABLE IF EXISTS "reply";
-CREATE TABLE IF NOT EXISTS "reply" (
-	"reply_UUID"	INTEGER NOT NULL UNIQUE,
-	"original_post_id"	INTEGER NOT NULL,
-	"reply_content"	TEXT NOT NULL,
-	"reply_user"	TEXT NOT NULL,
-	"reply_date"	TEXT NOT NULL,
-	FOREIGN KEY("original_post_id") REFERENCES "post"("UUID"),
-	PRIMARY KEY("reply_UUID")
 );
 INSERT INTO "user_access" ("username","password") VALUES ('steve','432c19c7ecdc9ef6884d01b21bd306ddd4b21d4ee139f10358cf5c1b7f113904');
 INSERT INTO "user_access" ("username","password") VALUES ('francis','432c19c7ecdc9ef6884d01b21bd306ddd4b21d4ee139f10358cf5c1b7f113904');
@@ -49,9 +52,9 @@ INSERT INTO "community" ("community") VALUES ('test');
 INSERT INTO "post" ("title","content","posting_user","posted_date","community","UUID") VALUES ('Second','Post','francis','2021-09-27 11:08:38.835803','test',1);
 INSERT INTO "post" ("title","content","posting_user","posted_date","community","UUID") VALUES ('Tastey','Treats','steve','2021-09-27 11:11:17.934762','test',2);
 INSERT INTO "post" ("title","content","posting_user","posted_date","community","UUID") VALUES ('nitrogen','sickness','jane','2021-09-27 11:12:34.648271','test',3);
-INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following") VALUES (1,'steve','','','','','');
-INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following") VALUES (2,'francis',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following") VALUES (3,'daphne',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following") VALUES (4,'jane',NULL,NULL,NULL,NULL,NULL);
 INSERT INTO "reply" ("reply_UUID","original_post_id","reply_content","reply_user","reply_date") VALUES (1,1,'Replyyyyiiiiinggg!','jane','2021-09-28 11:08:38.835803');
+INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following","tagline","date_joined","summary") VALUES (1,'steve','','','','','',NULL,'2021-09-27 11:08:38.835803',NULL);
+INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following","tagline","date_joined","summary") VALUES (2,'francis',NULL,NULL,NULL,NULL,NULL,NULL,'2021-09-27 11:08:38.835803',NULL);
+INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following","tagline","date_joined","summary") VALUES (3,'daphne',NULL,NULL,NULL,NULL,NULL,NULL,'2021-09-27 11:08:38.835803',NULL);
+INSERT INTO "user" ("id","username","avatar","first_name","last_name","communities","users_following","tagline","date_joined","summary") VALUES (4,'jane',NULL,NULL,NULL,NULL,NULL,NULL,'2021-09-27 11:08:38.835803',NULL);
 COMMIT;
