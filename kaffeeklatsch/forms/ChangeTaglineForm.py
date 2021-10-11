24476490e331d5070c120e25bcf0d83f1e4dd9e8
EmilyGodwin <ego3@umbc.edu>
2021-10-11 12:30:35 -0400

    some community content changes on feed

--- kaffeeklatsch/forms/ChangeTaglineForm.py
+++ kaffeeklatsch/forms/ChangeTaglineForm.py
@@ -5,5 +5,5 @@ from wtforms.validators import Length
 
 class ChangeTaglineForm(FlaskForm):
     #validation for tagline, requirement: length must be 0-20 characters
-    taglineChange = StringField('change your tagline:', validators =[Length(min=1, max=20, message='Your tagline cannot be longer than 20 characters!')], render_kw={"placeholder": "new Tagline"})
+    taglineChange = StringField('change your tagline:', validators =[Length(min=1, max=50, message='Your tagline cannot be longer than 50 characters!')], render_kw={"placeholder": "new Tagline"})
     submit = SubmitField('update tagline')
