b2c095b2af9e9aa1022cc9fa8663108ea61e7b9d
EmilyGodwin <89740394+EmGod35@users.noreply.github.com>
2021-10-12 15:45:35 -0400

    Update CommunityPainForm.py
    comment updates

--- kaffeeklatsch/forms/CommunityPainForm.py
+++ kaffeeklatsch/forms/CommunityPainForm.py
@@ -1,3 +1,8 @@
+#File: CommunityPainForm.py
+#@author: Ryan Dombek, Geoff Floding, David Hovey
+#Description: this file contains the form to submit
+#   from the community pane in feed
+
 # Imports
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextField
