2c9728e7bcf6795acc202f74277357f20f7279b3
EmilyGodwin <89740394+EmGod35@users.noreply.github.com>
2021-10-12 15:52:12 -0400

    Update SortPostForm.py
    comment updates

--- kaffeeklatsch/forms/SortPostForm.py
+++ kaffeeklatsch/forms/SortPostForm.py
@@ -1,3 +1,8 @@
+#File: SortPostForm.py
+#@author: Ryan Dombek, Geoff Floding, David Hovey
+#Description: this file contains the form to take int
+#   sorting filters from the sort bar
+
 # Imports
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
